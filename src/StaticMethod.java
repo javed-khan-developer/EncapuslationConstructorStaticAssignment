42e8f514e020f3df78df94ab5688385cfd3e3e80
khanjaved443 <57727397+khanjaved443@users.noreply.github.com>
2023-08-01 20:05:52 +0530

    static method done

---
+++ src/StaticMethod.java
@@ -0,0 +1,10 @@
+public class StaticMethod {
+    /* 4.Write a program to call an method without creating an object of a class */
+    static void show() {
+        System.out.println("static method...");
+    }
+
+    public static void main(String[] args) {
+        show();
+    }
+}
